2a138cd71dc68595dd8a1918f71c3103fbec7b4d
Felipe <felipebanfi84@gmail.com>
2019-07-20 15:15:25 -0300

    primera version

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,24 +1,51 @@
 //Package luhn implements functions related to Luhn formula
 package luhn
 
+import (
+  "unicode"
+)
 
-func spaceStringsBuilder(str string) string {
-    var b strings.Builder
-    b.Grow(len(str))
-    for _, ch := range str {
-        if !unicode.IsSpace(ch) {
-            b.WriteRune(ch)
-        }
-    }
-    return b.String()
-}
-
-
-//checks if a given string is valid per Luhn formula
-func Valid(input string) bool{
-  if len(input) <= 1{
+func Valid(input string) bool {
+  if len(input) <=1{
     return false
   }
 
-  return true
+  digits := []rune(input)
+	validCount := 0
+	sum := 0
+
+	for i := len(digits) - 1; i >= 0; i-- {
+		if unicode.IsSpace(digits[i]) {
+			continue
+		}
+
+		if !unicode.IsDigit(digits[i]) {
+			return false
+		}
+
+		sum += checksum(validCount, toInt(digits[i]))
+		validCount += 1
+	}
+
+	return validCount > 1 && sum%10 == 0
+}
+
+// converts rune digit to integer
+func toInt(digit rune) int {
+	return int(digit) - '0'
+}
+
+// computes the checksum for the digit
+func checksum(i int, digit int) int {
+	if i%2 == 0 {
+		return digit
+	}
+
+	digit *= 2
+
+	if digit > 9 {
+		digit -= 9
+	}
+
+	return digit
 }
