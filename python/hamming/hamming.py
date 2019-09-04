dbaea2c2f49e0457d7640178ff40546d43dc69f3
Felipe <felipebanfi84@gmail.com>
2019-09-04 20:01:12 -0300

    primera version

---
+++ python/hamming/hamming.py
@@ -0,0 +1,5 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("the DNA strands must be of equal length")
+
+    return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
