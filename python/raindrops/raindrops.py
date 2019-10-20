fa68b0e7b5246c22071a662f058139d10bb94e25
Felipe <felipebanfi84@gmail.com>
2019-10-20 11:58:40 -0300

    cambio diccionario por lista de tuplas dado que dependiendo la version de python las claves del dic pueden no estar en ornden

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,13 +1,13 @@
 
-FACTORS_RAINDROP_SOUND = {
-    3: 'Pling',
-    5: 'Plang',
-    7: 'Plong'
-}
+FACTORS_RAINDROP_SOUND = [
+    (3, 'Pling'),
+    (5, 'Plang'),
+    (7, 'Plong')
+]
 
 
 def convert(number):
-    result = [sound for factor, sound in FACTORS_RAINDROP_SOUND.items()
+    result = [sound for factor, sound in FACTORS_RAINDROP_SOUND
               if number % factor == 0]
 
     return ''.join(result) or str(number)
