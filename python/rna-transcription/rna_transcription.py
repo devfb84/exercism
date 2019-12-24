d41ce5b9efcc2153911d48ada85b3b5513105e89
Felipe <felipebanfi84@gmail.com>
2019-12-24 11:28:15 -0300

    primera iteracion

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,13 @@
+dna_to_rna = {"G": "C",
+              "C": "G",
+              "T": "A",
+              "A": "U"}
+
+
 def to_rna(dna_strand):
-    pass
+    rna = []
+
+    for char in dna_strand:
+        rna.append(dna_to_rna[char])
+
+    return "".join(rna)
