1fac143f72d29ee469aba537bf1a998e04eba0c7
Felipe <felipebanfi84@gmail.com>
2019-12-31 19:57:46 -0300

    iteracion 2, cambie la estructura para usar funciones

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,32 +1,19 @@
-def recite(start_verse, end_verse):
-    song = ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
-            "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, "
-            "and a Partridge in a Pear Tree.",
-            "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, "
-            "two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French "
-            "Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling "
-            "Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, "
-            "five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear "
-            "Tree.",
-            "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, "
-            "six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, "
-            "and a Partridge in a Pear Tree.",
-            "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, "
-            "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, "
-            "two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, "
-            "eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, "
-            "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, "
-            "nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, "
-            "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-            "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, "
-            "ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, "
-            "six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, "
-            "and a Partridge in a Pear Tree."]
+CARDINALS = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh",
+             "twelfth"]
+GIFTS = ["and a Partridge in a Pear Tree.", "two Turtle Doves", "three French Hens", "four Calling Birds",
+         "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming", "eight Maids-a-Milking",
+         "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"]
 
-    return song[start_verse-1:end_verse]
+
+def make_verse(start_verse):
+    day = CARDINALS[start_verse]
+    first_part = f"On the {day} day of Christmas my true love gave to me: "
+    rest = ", ".join(reversed(GIFTS[:start_verse + 1]))
+    verse = first_part + rest
+    if start_verse == 0:
+        verse = verse.replace('and ', '')
+    return verse
+
+
+def recite(start_verse, end_verse):
+    return [make_verse(verses) for verses in range(start_verse - 1, end_verse)]
